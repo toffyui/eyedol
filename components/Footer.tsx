b7fe845a47eda360dc00f65e95119eee41635717
yuikoito <yuiko.dev@gmail.com>
2021-12-06 04:03:44 +0900

    chore: add payment link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Flex } from "@chakra-ui/react";
+import { Box, Text, Flex, Image } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
@@ -13,11 +13,21 @@ const Footer = () => {
     >
       <Flex
         justifyContent="space-between"
+        alignItems="center"
         flexDirection={{ md: "row", base: "column" }}
       >
         <Text fontSize="sm">
           &copy; {new Date().getFullYear()} Eye Bigger All rights reserved.
         </Text>
+        <a href="https://www.buymeacoffee.com/yuikoito" target="_blank">
+          <Image
+            src={"/images/buyMeACoffee.svg"}
+            alt={"buy"}
+            borderRadius={"xl"}
+            h={"60px"}
+            cursor="pointer"
+          />
+        </a>
       </Flex>
     </Box>
   );
